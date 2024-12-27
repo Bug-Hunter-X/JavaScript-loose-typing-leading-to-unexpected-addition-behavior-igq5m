8b7ad8ecf59c2cb909786075e9e80e23fae1fe5d
BugHunter-X <askzenai@gmail.com>
2024-12-27 13:38:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,12 @@
+function foo(a, b) {
+  if (a === null || b === null) {
+    return 0; // Handle null values
+  }
+  //Explicit type conversion
+  return Number(a) + Number(b);
+}
+
+console.log(foo(null, 5)); // Output: 0
+console.log(foo(5, null)); // Output: 0
+console.log(foo(5, 5)); // Output: 10
+console.log(foo(5, '5')); //Output: 10
